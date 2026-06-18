749ef9d617eb13b30d96196eb01ebdfce98e29f0
IESUM <goni2727@naver.com>
2026-06-19 00:22:23 +0900

    commit practice

--- src/chapter01/Calculator.java
+++ src/chapter01/Calculator.java
@@ -2,6 +2,25 @@ package chapter01;
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Calculator practice");
+        System.out.println("Hello God of Java !!!!!");
+        System.out.println("I love this book.");
+    }
+    //빼기 메소드 만들기
+    public int subtract(int num1, int num2) {
+        int subtract;
+        subtract = num1 - num2;
+        return subtract;
+    }
+    //곱하기 메소드 만들기
+    public int multiply(int num1, int num2) {
+        int multiply;
+        multiply = num1 * num2;
+        return multiply;
+    }
+    //나누기 메소드 만들기
+    public int divide(int num1, int num2) {
+        int divide;
+        divide = num1 / num2;
+        return divide;
     }
 }
